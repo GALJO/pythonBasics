d1313cadb8f009821cdd2ad1b8d9e1b146fb4d32
John <jasiek.ole@gmail.com>
2020-06-14 21:20:52 +0200

    calculator fixed

--- calculator.py
+++ calculator.py
@@ -1,9 +1,17 @@
+def input_number(message):
+    print(message)
+    return float(input())
+
+
+def print_result(numb1, numb2, result, op):
+    print(str(numb1) + " " + op + " " + str(numb2) + " = " + str(result))
+
+
 print("Witaj w moim pierwszym kalkulatorze w języku Python!")
 print(" ")
-print("Podaj pierwszą liczbę:")
-one = float(input())
-print("Podaj drugą liczbę:")
-two = float(input())
+
+one = input_number("Podaj pierwszą liczbę")
+two = input_number("Podaj drugą liczbę")
 
 addition = one + two
 subtraction = one - two
@@ -11,8 +19,7 @@ multiplication = one * two
 division = one / two
 
 print("Twój wynik to:")
-print(str(one) + " + " + str(two) + " = " + str(addition))
-print(str(one) + " - " + str(two) + " = " + str(subtraction))
-print(str(one) + " * " + str(two) + " = " + str(multiplication))
-print(str(one) + " / " + str(two) + " = " + str(division))
-
+print_result(one, two, addition, "+")
+print_result(one, two, subtraction, "-")
+print_result(one, two, multiplication, "*")
+print_result(one, two, division, "/")
