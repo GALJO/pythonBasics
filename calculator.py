3f6c31c76b29e6c0ac48b020fb9a88a8aba9406e
John <jasiek.ole@gmail.com>
2020-06-15 20:20:22 +0200

    improved calculator.py

--- calculator.py
+++ calculator.py
@@ -1,25 +1,62 @@
 def input_number(message):
     print(message)
-    return float(input())
+    return input()
 
 
 def print_result(numb1, numb2, result, op):
     print(str(numb1) + " " + op + " " + str(numb2) + " = " + str(result))
 
 
+def finish_app(numb1):
+    if numb1 == "s":
+        print(" ")
+        print("Dziękuję za korzystanie z kalkulatora!")
+        exit(0)
+
+
+def is_number1_float(numb1):
+    try:
+        float(numb1)
+    except ValueError:
+        print("Nieprawidłowy pierwszy argument!")
+        exit(1)
+
+
+def is_number2_float(numb2):
+    try:
+        float(numb2)
+    except ValueError:
+        print("Nieprawidłowy drugi argument!")
+        exit(1)
+
+
+def validation(numb1, numb2):
+    is_number1_float(numb1)
+    is_number2_float(numb2)
+
+
 print("Witaj w moim pierwszym kalkulatorze w języku Python!")
-print(" ")
 
-one = input_number("Podaj pierwszą liczbę")
-two = input_number("Podaj drugą liczbę")
+x = 0
 
-addition = one + two
-subtraction = one - two
-multiplication = one * two
-division = one / two
+while x < 10:
+    print(" ")
+    one = input_number("Podaj pierwszą liczbę (aby zakończyć 's')")
+    finish_app(one)
+    two = input_number("Podaj drugą liczbę")
 
-print("Twój wynik to:")
-print_result(one, two, addition, "+")
-print_result(one, two, subtraction, "-")
-print_result(one, two, multiplication, "*")
-print_result(one, two, division, "/")
+    validation(one, two)
+
+    format_one = float(one[0:])
+    format_two = float(two[0:])
+
+    addition = format_one + format_two
+    subtraction = format_one - format_two
+    multiplication = format_one * format_two
+    division = format_one / format_two
+
+    print("Twój wynik to:")
+    print_result(one, two, addition, "+")
+    print_result(one, two, subtraction, "-")
+    print_result(one, two, multiplication, "*")
+    print_result(one, two, division, "/")
