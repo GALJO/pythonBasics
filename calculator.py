74ca9d19f25d077b8439273535d154804f14e73e
GALJO <jasiek.ole@gmail.com>
2020-06-20 15:09:35 +0200

    No próbowałem no

--- calculator.py
+++ calculator.py
@@ -1,65 +1,91 @@
-# TODO refactor and update calculator
-
-
-def input_number(message):
-    print(message)
-    return input()
-
-
 def print_result(numb1, numb2, result, op):
     print(str(numb1) + " " + op + " " + str(numb2) + " = " + str(result))
 
 
-def finish_app(numb1):
-    if numb1 == "s":
+def finish_app():
+    if numbers == "s":
+        return True
+    return False
+
+
+def is_number1_float():
+    try:
+        float(one)
+    except ValueError:
+        return False
+    return True
+
+
+def is_number2_float():
+    try:
+        float(two)
+    except ValueError:
+        return False
+    return True
+
+
+def len_valid():
+    if int(len(numbers)) >= 5:
+        return True
+    return False
+
+
+def symb_valid():
+    if symb == "+":
+        return True
+    if symb == "-":
+        return True
+    if symb == "*":
+        return True
+    if symb == "/":
+        return True
+    return False
+
+
+def calculate():
+    result = None
+    if symb == "+":
+        result = one + two
+    if symb == "-":
+        result = one - two
+    if symb == "*":
+        result = one * two
+    if symb == "/":
+        result = one / two
+    return result
+
+
+print("Witaj w moim kalkulatorze w języku Python!")
+
+while True:
+    print(" ")
+    print("Wprowadź działanie (możesz dodawać: '+' odejmować: '-' mnożyć: '*' dzielić: '/'  lub wyjść: 's'):")
+
+    numbers = input()
+    if finish_app():
         print(" ")
         print("Dziękuję za korzystanie z kalkulatora!")
         exit(0)
 
+    if not len_valid():
+        print("Za mało argumentów!")
+        continue
 
-def is_number1_float(numb1):
-    try:
-        float(numb1)
-    except ValueError:
-        print("Nieprawidłowy pierwszy argument!")
-        exit(1)
+    one = numbers[0]
+    symb = str(numbers[2])
+    two = numbers[4]
 
+    if not is_number1_float():
+        print("Niepoprawny pierwszy argument!")
+        continue
+    if not is_number2_float():
+        print("Niepoprawny drugi argument!")
+        continue
+    if not symb_valid():
+        print("Podałeś niepoprawny symbol!")
+        continue
 
-def is_number2_float(numb2):
-    try:
-        float(numb2)
-    except ValueError:
-        print("Nieprawidłowy drugi argument!")
-        exit(1)
+    one = float(one)
+    two = float(two)
 
-
-def validation(numb1, numb2):
-    is_number1_float(numb1)
-    is_number2_float(numb2)
-
-
-print("Witaj w moim pierwszym kalkulatorze w języku Python!")
-
-x = 0
-
-while x < 10:
-    print(" ")
-    one = input_number("Podaj pierwszą liczbę (aby zakończyć 's')")
-    finish_app(one)
-    two = input_number("Podaj drugą liczbę")
-
-    validation(one, two)
-
-    format_one = float(one[0:])
-    format_two = float(two[0:])
-
-    addition = format_one + format_two
-    subtraction = format_one - format_two
-    multiplication = format_one * format_two
-    division = format_one / format_two
-
-    print("Twój wynik to:")
-    print_result(one, two, addition, "+")
-    print_result(one, two, subtraction, "-")
-    print_result(one, two, multiplication, "*")
-    print_result(one, two, division, "/")
+    print("{} {} {} = {}".format(one, symb, two, calculate()))
