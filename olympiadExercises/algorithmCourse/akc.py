cdd959c3ac6271fabf70436590ba010a28398367
John <jasiek.ole@gmail.com>
2020-08-10 23:33:57 +0200

    Done 'akcelerator' exercise

--- olympiadExercises/algorithmCourse/akc.py
+++ olympiadExercises/algorithmCourse/akc.py
@@ -1,17 +1,52 @@
 n = int(input())  # Liczba cząstek
 speed_of_particles = input().split()
 q = int(input())  # Liczba zapytań
-inquiry_speed = int(input())
+inquiry_speeds = []
+results = []
 
-start = 0
-end = len(speed_of_particles)
 
-done = False
-count = 0
-while not done:
-    center = int((start + end + 1) / 2)
-    place = speed_of_particles[center]
-    if place == inquiry_speed:
-        count += 1
-        start = center
-    if start
+def amount_of_occurrences(_speed_of_particles, _place, _center):
+    _count = 0
+    for i in range(_center, len(_speed_of_particles)):
+        if int(_speed_of_particles[i]) == _place:
+            _count += 1
+        else:
+            break
+    for i in range(_center - 1, 0, -1):
+        if int(_speed_of_particles[i]) == _place:
+            _count += 1
+        else:
+            break
+    return _count
+
+
+def find_speed(_x, _speed_of_particles):
+    count = 0
+    done = False
+    start = 0
+    end = len(_speed_of_particles) - 1
+
+    while not done:
+        center = int((start + end) / 2)
+        place = int(_speed_of_particles[center])
+
+        if place == _x:
+            count = amount_of_occurrences(_speed_of_particles, place, center)
+            done = True
+        elif start >= end:
+            done = True
+        elif place > _x:
+            end = center - 1
+        elif place < _x:
+            start = center + 1
+    return count
+
+
+for i in range(q):
+    inquiry_speeds.append(int(input()))
+
+for i in range(q):
+    results.append(find_speed(inquiry_speeds[i], speed_of_particles))
+
+for result in results:
+    print(result)
