b05715a843d805dc643bd25363d8d3f18580b276
John <jasiek.ole@gmail.com>
2020-11-14 13:29:59 +0100

    zmi done

--- olympiad_exercises/zmi.py
+++ olympiad_exercises/zmi.py
@@ -1,1 +1,10 @@
-x
+word = list(map(str, input()))
+count = 0
+if len(word) == 1:
+    count += 0
+else:
+    for i in range(1, len(word)):
+        if word[i] == word[i - 1]:
+            count += 1
+
+print(count)
