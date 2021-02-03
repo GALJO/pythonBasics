2dab1a2a4df8047ca876259139d46370b9463bb8
John <jasiek.ole@gmail.com>
2021-02-03 17:11:54 +0100

    bas and ser done, hur repaired

---
+++ olympiad_exercises/okiXIV/stage1/open_tour/bas.py
@@ -0,0 +1,26 @@
+def main():
+    _res = 1
+    _i = 1
+    while _i < n:
+        if P[_i - 1] <= P[_i]:
+            _i += 1
+            continue
+        else:
+            _res += 1
+            _j = _i
+            while _j < n:
+                if P[_j - 1] >= P[_j]:
+                    _j += 1
+                    continue
+                else:
+                    _res += 1
+                    _i = _j
+                    break
+            _i = _j
+        _i += 1
+    return _res
+
+
+n = int(input())
+P = list(map(int, input().split()))
+print(main())
--- olympiad_exercises/okiXIV/stage1/open_tour/hur.py
+++ olympiad_exercises/okiXIV/stage1/open_tour/hur.py
@@ -1,9 +1,14 @@
 def main():
     for _i in range(1, n + 1):
+        if _i % 11 == 0 and _i % 7 == 0:
+            print('Wiwat!')
+            continue
         if _i % 7 == 0:
             print('Hurra!')
+            continue
         if _i % 11 == 0:
             print('Super!')
+            continue
         if _i % 7 != 0 and _i % 11 != 0:
             print(_i)
 
